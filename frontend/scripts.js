695e18905f022034a7c39c625cd6be903b599de9
Zackary Roche <zack140821@francemel.fr>
2026-07-07 23:41:10 +0200

    Ajout des informations du serveurs
    Ajout d'un compteur de membre, de Staff ainsi que l'affichage de l'ouverture. Les données doivent être mises manuellement en attendant un backend et peuvent donc être factices pour certaines et erronées pour d'autres.
    Reste à faire sur la partie info :
    - Mise en forme

---
+++ frontend/scripts.js
@@ -0,0 +1,16 @@
+// Information du serveur
+
+// Import des constantes d'informations
+const memberCount = document.getElementById('member-counts');
+const staffCount = document.getElementById('staff-counts');
+const openDate = document.getElementById('open-date');
+
+// Définition des constantes
+const manuelMemberCount = 25;
+const manuelStaffCount = 3;
+const manuelOpenDate = "1/08/2026";
+
+// Application des constantes
+memberCount.innerText = manuelMemberCount;
+staffCount.innerText = manuelStaffCount;
+openDate.innerText = manuelOpenDate;
